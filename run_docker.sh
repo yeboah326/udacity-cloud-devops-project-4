2858e93f90d0c27a79ea3578fb9d3d5b3bdce3a8
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-08-01 13:59:17 +0000

    Add code to generate log when prediction is returned

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=sklearn-api .
+docker build --tag=ml-api .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8080:80 sklearn-api
+docker run -p 8080:80 ml-api
